5c0f2324f37ed6fb28141d67df5ca548cc31a751
Adam <regality@gmail.com>
2012-01-26 10:37:34 -0700

    in spite of the pure awesomeness of keybard cats, we should throw errors

--- controllers/errors.js
+++ controllers/errors.js
@@ -6,28 +6,48 @@ exports.init = function(app) {
     Error.captureStackTrace(this, arguments.callee);
   }
   NotFound.prototype.__proto__ = Error.prototype;
-	
+
   // 500 Page
-  app.use(function(err, req, res, next){
-    throw new Error('keyboard cat!');
+  app.use(function(err, req, res, next) {
+    throw err;
   });
-	
+
   // 404 page
-  app.use(function(req, res){
+  app.use(function(req, res) {
     throw new NotFound;
   });
-	
+
   //setup the errors
-  app.error(function(err, req, res, next){
+  app.error(function(err, req, res, next) {
+     var defaultError = "An error occured.";
     if (err instanceof NotFound) {
       res.render('errors/404', { locals: { 
         title : '404 - Not Found'
       },status: 404 });
     } else {
+      if (typeof err == "object") {
+         if (err.hasOwnProperty("message")) {
+            message = err.message;
+         } else {
+            message = defaultError;
+         }
+         if (err.hasOwnProperty("log")) {
+            log = err.log;
+         } else {
+            log = message;
+         }
+      } else if (typeof err == "string") {
+         message = err;
+         log = err;
+      } else {
+         message = defaultError;
+         log = defaultError;
+      }
       res.render('errors/500', { locals: { 
         title : 'The Server Encountered an Error',
-        error: err},
+        error: message},
         status: 500 });
+      console.log("ERROR: " + log);
     }
   });
 }

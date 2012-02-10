0684becc2b3e83ad946732af81ee943693c63a35
Adam <regality@gmail.com>
2012-02-10 16:18:04 -0700

    [fixes #24792801] We are now using confrodo for configuration.
    rb: Chase Brammer

--- config/index.js
+++ config/index.js
@@ -1,29 +1,8 @@
-// Setup config to use in order:
-//  1. Command-line arguments
-//  2. Environment variables
-//  3. Values in `config.json`
+// Setup config
 
-var optimist = require('optimist')
-  , argv = optimist.argv
-  , env
-  , filename
-  , config;
-
-if (argv.hasOwnProperty('env')) {
-  env = argv.env;
-} else if (typeof process.env.NODE_ENV === 'string') {
-  env = process.env.NODE_ENV;
-} else {
-  env = 'development';
-}
-
-filename = __dirname + '/' + env;
-config = require(filename);
-for (var i in argv) {
-  if (i !== '_' && i !== '$0' && argv.hasOwnProperty(i)) {
-    config[i] = argv[i];
-  }
-}
-config.env = env;
+var confrodo = require('confrodo') // one ring to rule them all
+  , filename = __dirname + '/' + confrodo.env + '.json'
+  , defaults = {}
+  , config = confrodo(defaults, filename, "ARGV");
 
 module.exports = config;

4e5b444bc9c9e7175da3f7229833bfcca14bfcc1
Gary Rogers <garyr@mediarain.com>
2012-02-23 11:32:20 -0700

    Implements ifit-auth against the ifit authentication server
    * Removes mongoose-auth and everyauth
    * Temporarily removes facebook login support
    * Uses new private module ifit-auth for npm (see package.json)
    
    NOTES:
    - Authentication now uses a shared cookie across a cookie domain (see config file)
    - You must configure your dev env to use a hostname compatible with the cookie domain (e.g. johnd.ifit-dev.com -> 127.0.0.1 via hosts file)
    - You must now point to the AWS dev redis server for use of the shared session:
    
    	redis://redis.db.ifit-dev.com:6379
    
    - You must use the same shared session secret as the authentication server for use in decrypting cookies.  Please use the current value in the development.json config file
    - See controllers/root.js - secret route for example on how to get user data

--- controllers/root.js
+++ controllers/root.js
@@ -1,3 +1,5 @@
+var ifitAuth = require('ifit-auth');
+var util = require('util');
 
 // Init the base routes of the application
 var home = function (req, res) {
@@ -5,12 +7,24 @@ var home = function (req, res) {
 };
 
 var logout = function (req, res) {
-  req.logout();
+  ifitAuth.logout(req, res, false);
   res.redirect('/');
+};
+
+var secret = function (req, res) {
+  var me = {};
+  // if you need data from /me in the browser, just make an XHR to https://[AUTH_URI]/me
+  ifitAuth.me(req.headers, function(_me, _res) {
+    if(!_me) return;
+    me = _me;
+    console.log(me.email);
+  });
+  res.render('secret');
 };
 
 exports.init = function (app) {
   app.get('/', home);
   app.get('/logout', logout);
+  app.get('/secret', secret);
 }
 
